557aca828f06dd04b5ec9e3906bd9f3103bf5b58
Jordan (CmdCtrl) <jordan.lyon@rutgers.edu>
2016-12-02 01:31:22 -0500

    This would be easier if you would just die - Park

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -8,6 +8,7 @@ public class Bartok : MonoBehaviour {
 	public TextAsset layoutXML;
 	public Vector3 layoutCenter = Vector3.zero;
 
+	public float handfanDegrees = 10f;
 	public bool ______________;
 
 	public Deck deck;
@@ -16,6 +17,8 @@ public class Bartok : MonoBehaviour {
 
 	public BartokLayout layout;
 	public Transform layoutAnchor;
+	public List<Player> players;
+	public CardBartok targetCard;
 
 	void Awake(){
 		S = this;
@@ -31,6 +34,7 @@ public class Bartok : MonoBehaviour {
 		layout.ReadLayout (layoutXML.text);
 
 		drawPile = UpgradeCardsList (deck.cards);
+		LayoutGame ();
 
 	}
 
@@ -41,4 +45,67 @@ public class Bartok : MonoBehaviour {
 		}
 		return (lCB);
 	}
+
+	public void ArrangeDrawPile(){
+		CardBartok tCB;
+
+		for (int i = 0; i < drawPile.Count; i++) {
+			tCB = drawPile [i];
+			tCB.transform.parent = layoutAnchor;
+			tCB.transform.localPosition = layout.drawPile.pos;
+			tCB.faceUp = false;
+			tCB.SetSortingLayerName (layout.drawPile.layerName);
+			tCB.SetSortOrder (-i * 4);
+			tCB.state = CBState.drawpile;
+		}
+
+	}
+
+	void LayoutGame(){
+		if (layoutAnchor == null) {
+			GameObject tGO = new GameObject ("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		ArrangeDrawPile ();
+
+		Player pl;
+		players = new List<Player> ();
+		foreach (SlotDef tSD in layout.slotDefs) {
+			pl = new Player ();
+			pl.handSlotDef = tSD;
+			players.Add (pl);
+			pl.playerNum = players.Count;
+		}
+
+		players [0].type = PlayerType.human;
+	}
+
+	public CardBartok Draw(){
+		CardBartok cd = drawPile [0];
+		drawPile.RemoveAt (0);
+		return(cd);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			players[0].AddCard(Draw ());
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			players[1].AddCard(Draw());
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			players[2].AddCard(Draw());
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			players[3].AddCard(Draw());
+		}
+	}
+
 }
